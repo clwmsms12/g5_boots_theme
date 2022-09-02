1f4e1db847959b5c9ae126eeb70b98af4e4fda68
clwmsms12 <clwmsms12@naver.com>
2022-09-02 13:30:03 +0900

    btbbb0902

--- yboots/index.php
+++ yboots/index.php
@@ -40,7 +40,105 @@ include_once(G5_THEME_PATH.'/head.php');
     .item01{top: -60px;right: 50px;}
     .item02{right: 0;bottom: 0;}
     .item03{width: 180px;left: 0;bottom: 0;}
+
+
+
+    .cont{background: skyblue;}
 </style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 p-4">
+            <?php
+                // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
+                // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
+                // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
+                echo latest('theme/basic', 'notice', 5, 23);		// 최소설치시 자동생성되는 갤러리게시판
+            ?>
+        </div>
+        <div class="col-md-6 p-4">
+            <?php
+                // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
+                // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
+                // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
+                echo latest('theme/basic', 'notice', 5, 23);		// 최소설치시 자동생성되는 갤러리게시판
+            ?>
+        </div>
+    </div>
+</div>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<style>
+    .prev,.next{cursor: pointer;}
+    img.prev,img.next{position: absolute;top: 50%;transform:translateY(-50%)}
+    img.prev{left: -50px;}
+    img.next{right: -50px;}
+    .slickslide1 .sItem{
+        padding: 10px;opacity: 0.5;transition:0.3s;transform:scale(0.8)
+    }
+    .slickslide1 .sItem.slick-center{opacity: 1;transform:scale(1)}
+</style>
+
+<div class="container position-relative">
+    <img src="<? echo G5_THEME_IMG_URL ?>/LL.png" alt="" class="prev d-none d-md-block">
+    <div class="slickslide1">
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img1.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img2.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img3.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img4.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img5.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img6.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="sItem">
+            <img src="<? echo G5_THEME_IMG_URL ?>/img7.jpg" alt="" class="img-fluid">
+        </div>
+    </div>
+    <img src="<? echo G5_THEME_IMG_URL ?>/RR.png" alt="" class="next d-none d-md-block">
+</div>
+
+<script>
+    $('.slickslide1').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  nextArrow:$('.next'),
+  prevArrow:$('.prev'),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+</script>
+
 <div class="container py-5">
     <div class="row">
         <div class="col-md-6 position-relative d-flex justify-content-center align-items-center">
