73261cb00c30d51bf9c9be51a7173ba31750b521
clwmsms12 <clwmsms12@naver.com>
2022-09-15 18:18:23 +0900

    gnfoot

--- yboots/tail.php
+++ yboots/tail.php
@@ -20,12 +20,42 @@ if(G5_COMMUNITY_USE === false) {
 <hr>
 
 <!-- 하단 시작 { -->
+<style>
+    .footer{background: #3e3e46;}
+    .fMenubar>.conT{width: 100%;}
+    .fMenubar>li>a{color:#fff;}
+    .fMenubar>li>.sMenu{margin-top: 15px;}
+    .fMenubar>li>.sMenu>li{margin-bottom:10px;}
+    .fMenubar>li>.sMenu>li>a{color:#acacb0;font-size:14px}
+</style>
 
-<div class="bg-dark">
-    <div class="container py-5 text-center text-white">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, repellat.
+<footer class="footer py-5">
+    <div class="container">
+        <ul class="fMenubar d-flex justify-content-between">
+            <li class="conT">
+                <a href="#" class="pb-3">YBM Lemon 스토리</a>
+                <ul class="sMenu">
+                    <li class="conpT"><a href="#">상담신청</a></li>
+                </ul>
+            </li>
+            <li class="conT">
+                <a href="#" class="pb-3">수강신청</a>
+                <ul class="sMenu">
+                    <li class="conpT"><a href="#">수강신청</a></li>
+                    <li class="conpT"><a href="#">무료체험</a></li>
+                    <li class="conpT"><a href="#">레벨테스트</a></li>
+                    <li class="conpT"><a href="#">무료체험&레벨테스트 신청확인</a></li>
+                </ul>
+            </li>
+            <h2 class="conT"><a href="#">커뮤니티</a></h2>
+            <h2 class="conT"><a href="#">마이클래스</a></h2>
+            <h2 class="conT"><a href="#">My Lemon</a></h2>
+            <h2 class="conT"><a href="#">고객센터</a></h2>
+        </ul>
     </div>
-</div>
+</footer>
+
+
 
 <?php
 if(G5_DEVICE_BUTTON_DISPLAY && !G5_IS_MOBILE) { ?>
