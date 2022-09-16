30daa5f6567242914ef3221c7b74910a4d399468
clwmsms12 <clwmsms12@naver.com>
2022-09-16 18:17:03 +0900

    btbt

--- yboots/tail.php
+++ yboots/tail.php
@@ -27,9 +27,18 @@ if(G5_COMMUNITY_USE === false) {
     .fMenubar>li>.sMenu{margin-top: 15px;}
     .fMenubar>li>.sMenu>li{margin-bottom:10px;}
     .fMenubar>li>.sMenu>li>a{color:#acacb0;font-size:14px}
+
+    .footer2{background: #232328;}
+    .fMenu>li{color:#acacb0;}
+    .fMenu>li>a{color:#fff;font-size:17px}
+    .fIcon img{width: 40px;height: 40px;}
+    .footLine{border-bottom:1px solid #acacb0;}
+
+    .footer2 p{color:#acacb0;}
+    .bold{font-weight:bold;font-size:15px}
 </style>
 
-<footer class="footer py-5">
+<footer class="footer py-4">
     <div class="container">
         <ul class="fMenubar d-flex justify-content-between">
             <li class="conT">
@@ -47,14 +56,95 @@ if(G5_COMMUNITY_USE === false) {
                     <li class="conpT"><a href="#">무료체험&레벨테스트 신청확인</a></li>
                 </ul>
             </li>
-            <h2 class="conT"><a href="#">커뮤니티</a></h2>
-            <h2 class="conT"><a href="#">마이클래스</a></h2>
-            <h2 class="conT"><a href="#">My Lemon</a></h2>
-            <h2 class="conT"><a href="#">고객센터</a></h2>
+            <li class="conT">
+                <a href="#" class="pb-3">커뮤니티</a>
+                <ul class="sMenu">
+                    <li class="conpT"><a href="#">Gallery</a></li>
+                    <li class="conpT"><a href="#">Lemon Seed Mall</a></li>
+                    <li class="conpT"><a href="#">이벤트</a></li>
+                </ul>
+            </li>
+            <li class="conT">
+                <a href="#" class="pb-3">마이클래스</a>
+                <ul class="sMenu">
+                    <li class="conpT"><a href="#">Calendar</a></li>
+                    <li class="conpT"><a href="#">숙제 제출 및 확인</a></li>
+                    <li class="conpT"><a href="#">Lemon Seed</a></li>
+                    <li class="conpT"><a href="#">수강현황</a></li>
+                    <li class="conpT"><a href="#">정회원 공지사항</a></li>
+                </ul>
+            </li>
+            <li class="conT">
+                <a href="#" class="pb-3">My Lemon</a>
+                <ul class="sMenu">
+                    <li class="conpT"><a href="#">수강신청 내역</a></li>
+                    <li class="conpT"><a href="#">포인트몰 구매내역</a></li>
+                    <li class="conpT"><a href="#">학부모정보</a></li>
+                    <li class="conpT"><a href="#">학습자정보</a></li>
+                    <li class="conpT"><a href="#">1:1답변확인</a></li>
+                    <li class="conpT"><a href="#">학습장애답변확인</a></li>
+                    <li class="conpT"><a href="#">상담신청내역</a></li>
+                </ul>
+            </li>
+            <li class="conT">
+                <a href="#" class="pb-3">고객센터</a>
+                <ul class="sMenu">
+                    <li class="conpT"><a href="#">공지사항</a></li>
+                    <li class="conpT"><a href="#">FAQ</a></li>
+                    <li class="conpT"><a href="#">1:1상담</a></li>
+                    <li class="conpT"><a href="#">학습장애신고</a></li>
+                </ul>
+            </li>
         </ul>
     </div>
 </footer>
 
+<footer class="footer2">
+    <div class="footLine">
+        <div class="container">
+            <div class="fTop py-2 d-flex justify-content-between">
+                <ul class="fMenu d-flex align-items-center gap-3">
+                    <li class="conT"><a href="#">로그인</a></li>
+                    <li>|</li>
+                    <li class="conT"><a href="#">이용약관</a></li>
+                    <li>|</li>
+                    <li class="conT"><a href="#">개인정보처리방침</a></li>
+                    <li>|</li>
+                    <li class="conT"><a href="#">영상정보처리기기 운영관리방침</a></li>
+                </ul>
+                <ul class="fIconwrap d-flex gap-2">
+                    <li class="fIcon"><img src="<? echo G5_THEME_IMG_URL ?>/naver_blog2.png" alt=""></li>
+                    <li class="fIcon"><img src="<? echo G5_THEME_IMG_URL ?>/Instagram_Glyph_Gradient_RGB.png" alt=""></li>
+                    <li class="fIcon"><img src="<? echo G5_THEME_IMG_URL ?>/youtube_social_icon_red.png" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container py-4">
+        <div class="fBottom d-flex align-items-center gap-2 mb-4">
+            <p class="bold">사업자등록번호</p>
+            <p>101-81-92326</p>
+            <p class="bold">대표이사</p>
+            <p>홍길동</p>
+        </div>
+        <div class="fBottom d-flex align-items-center gap-3 mb-4">
+            <p class="bold">통신판매신고번호</p>
+            <p>제 01-985호</p>
+            <p class="bold">TEL</p>
+            <p>1670-7978</p>
+            <p class="bold">FAX</p>
+            <p>02-2260-4410</p>
+            <p class="bold">E-mail</p>
+            <p>ybmky@ybm.co.kr</p>
+        </div>
+        <div class="fBottom d-flex align-items-center gap-3 mb-4">
+            <p class="bold">서울특별시 종로구 창경굴로 112-7(주)YBM교육</p>
+        </div>
+        <div class="fBottom d-flex align-items-center gap-3">
+            <p>Copyrightⓒ(주)YBM교육. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
 
 
 <?php
